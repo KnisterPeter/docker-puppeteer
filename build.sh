4fd08a36007174f42612a5c1d0135a5a520a2f42
Markus Wolf <markus.wolf@magicline.de>
2017-11-23 14:18:49 +0100

    fix: push all tags

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t knisterpeter/docker-puppeteer .
 docker tag knisterpeter/docker-puppeteer knisterpeter/docker-puppeteer:$(git log --oneline -n 1 |cut -f 1 -d " ")
 docker tag knisterpeter/docker-puppeteer knisterpeter/docker-puppeteer:latest
-docker push knisterpeter/docker-puppeteer:latest
+docker push knisterpeter/docker-puppeteer
